dbfded8fab018d94c6c62507031cda77d85dbc47
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-01-28 11:24:02 +0000

    added lock and unlock tests

--- secret_diary/lib/lock.rb
+++ secret_diary/lib/lock.rb
@@ -1,3 +1,18 @@
 class Lock 
 
+  def initialize
+    @locked = true
+  end
+
+  def lock
+    @locked = true
+  end
+
+  def unlock
+    @locked = false
+  end
+
+  def locked?
+    @locked
+  end
 end
--- secret_diary/spec/lock_spec.rb
+++ secret_diary/spec/lock_spec.rb
@@ -1,5 +1,23 @@
 require_relative '../lib/lock'
 
 describe Lock do
-  
+
+  it { should respond_to(:lock) }
+  it { should respond_to(:unlock) }
+  it { should respond_to(:locked?) }
+
+  describe "#lock" do
+    it "locks the lock" do
+      subject.lock
+      expect(subject).to be_locked
+    end
+  end
+
+  describe "#unlock" do
+    it "unlocks the lock" do
+      subject.unlock
+      expect(subject).not_to be_locked
+    end
+  end
+
 end
